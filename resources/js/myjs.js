680bf3fd4d3503d4277de95eb341519776b63b53
Mir Tanvir <tanvir98.mt@gmail.com>
2020-02-01 00:04:03 +0600

    done

---
+++ resources/js/myjs.js
@@ -0,0 +1,123 @@
+var timer_start_time = -1;
+var current_time;
+
+function displayClock() {
+	var today = new Date();
+	var hour_in_24hr = today.getHours();
+	var hour_in_12hr = hour_in_24hr % 12;
+	var minute = today.getMinutes();
+	var second = today.getSeconds();
+	var am_pm = hour_in_24hr > 11 ? 'pm' : 'am';
+	minute = checkTime(minute);
+	second = checkTime(second);
+	document.title = hour_in_12hr + ':' + minute + ' ' + am_pm;
+	document.getElementById('clock').innerHTML = hour_in_12hr + ':' + minute + ':' + second + ' ' + am_pm;
+	document.getElementById('clock_fullscreen').innerHTML = hour_in_12hr + ':' + minute + ':' + second + ' <small>' + am_pm + '</small>';
+
+	var t = setTimeout(displayClock, 500);
+}
+
+function checkTime(input) {
+	if (input < 10) {
+		input = '0' + input;
+	} // add zero in front of numbers < 10
+	return input;
+}
+
+function startTimer() {
+	console.log('startTimer');
+
+	if (timer_start_time < 0) {
+		console.log('set timer_start_time');
+		timer_start_time = new Date();
+	}
+
+	var current_time = new Date();
+
+	//console.log(current_time + ' ' + timer_start_time);
+
+	var res = Math.abs(current_time - timer_start_time) / 1000;
+
+	// get total days between two dates
+	var days = Math.floor(res / 86400);
+	//console.log('<br>Difference (Days): ' + days);
+
+	// get hours
+	var hours = Math.floor(res / 3600) % 24;
+	//console.log('<br>Difference (Hours): ' + hours);
+
+	// get minutes
+	var minutes = Math.floor(res / 60) % 60;
+	//console.log('<br>Difference (Minutes): ' + minutes);
+
+	// get seconds
+	var seconds = Math.floor(res % 60);
+	//console.log('<br>Difference (Seconds): ' + seconds);
+
+	document.getElementById('timer').innerHTML = hours + ' h : ' + minutes + ' m : ' + seconds + ' s';
+
+	document.getElementById('timer_fullscreen').innerHTML = hours + ' <small>h</small> : ' + minutes + ' <small>m</small> : ' + seconds + ' <small>s</small>';
+
+	document.getElementById('timer_mini_view').innerHTML = hours + ' h : ' + minutes + ' m : ' + seconds + ' s';
+
+	//c = diff_minutes(current_time, timer_start_time);
+	//console.log(c);
+
+	var t2 = setTimeout(startTimer, 500);
+}
+
+/* Get the element you want displayed in fullscreen mode (a video in this example): */
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+                             Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+	fullscreen('clock_fullscreen_div');
+
+	var elem = document.getElementById('StopwatchModal');
+
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.mozRequestFullScreen) {
+		/* Firefox */
+		elem.mozRequestFullScreen();
+	} else if (elem.webkitRequestFullscreen) {
+		/* Chrome, Safari and Opera */
+		elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) {
+		/* IE/Edge */
+		elem.msRequestFullscreen();
+	}
+}
+
+function diff_minutes(date2, date1) {
+	var res = Math.abs(date1 - date2) / 1000;
+
+	// get total days between two dates
+	var days = Math.floor(res / 86400);
+	console.log('<br>Difference (Days): ' + days);
+
+	// get hours
+	var hours = Math.floor(res / 3600) % 24;
+	console.log('<br>Difference (Hours): ' + hours);
+
+	// get minutes
+	var minutes = Math.floor(res / 60) % 60;
+	console.log('<br>Difference (Minutes): ' + minutes);
+
+	// get seconds
+	var seconds = res % 60;
+	console.log('<br>Difference (Seconds): ' + seconds);
+}
+
+function fullscreen(target) {
+	document.getElementById('main').style.display = 'none';
+	document.getElementById(target).style.display = 'block';
+}
+
+document.onkeydown = function keyPress(e) {
+	if (e.key === 'Escape') {
+		document.getElementById('main').style.display = 'block';
+		document.getElementById('clock_fullscreen_div').style.display = 'none';
+		document.getElementById('timer_fullscreen_div').style.display = 'none';
+	}
+};
