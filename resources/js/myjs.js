7d42c4736f82ca0f4702bc653744719b99fba4cf
Mir Tanvir <tanvir98.mt@gmail.com>
2020-07-30 02:39:40 +0600

    Created Timer feature v1

--- resources/js/myjs.js
+++ resources/js/myjs.js
@@ -42,5 +42,62 @@ Clock.prototype.display = function() {
     return hour + ':' + minute + ':' + second + ' ' + am_pm;
 }
 
+
+function Timer() {
+    this.start_time = null;
+    this.history = 30;
+    this.active = False;
+}
+
+Timer.prototype.start = function() {
+    this.start_time = new Date();
+    this.active = True;
+}
+
+Timer.prototype.active = function() {
+    return this.active;
+}
+
+Timer.prototype.display = function() {
+    const current_time = new Date();
+    if(this.active == True) {
+        start_time = this.start_time;
+    } else {
+        start_time = new Date();
+    }
+    var diff = ((current_time.getTime() - start_time.getTime()) / 1000) + this.history;
+    var diff_hour = Math.floor(diff / 3600);
+    diff = diff % 3600;
+    var diff_minute = Math.floor(diff / 60);
+    var diff_second = Math.floor(diff % 60);
+
+    if (diff_hour < 10) {diff_hour = "0" + diff_hour;}
+    if (diff_minute < 10) {diff_minute = "0" + diff_minute;}
+    if (diff_second < 10) {diff_second = "0" + diff_second;}
+    if (diff_hour > 0) {
+        return diff_hour + ':' + diff_minute + ':' + diff_second;
+    }
+    else {
+        return diff_minute + ':' + diff_second;
+    }
+}
+
+Timer.prototype.pause = function() {
+    const current_time = new Date();
+    if(this.start_time != null) {
+        start_time = this.start_time;
+    } else {
+        start_time = new Date();
+    }
+    var diff = ((current_time.getTime() - start_time.getTime()) / 1000);
+    this.history += diff;
+    this.start_time = null;
+}
+
+
 clock = new Clock();
 console.log(clock.display());
+
+timer = new Timer();
+timer.start();
+console.log(timer.display());
