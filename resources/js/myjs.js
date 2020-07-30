a830a687583b3a86c9fdc622070d1c29c57ba188
Mir Tanvir <tanvir98.mt@gmail.com>
2020-07-30 20:45:12 +0600

    Window before close alert added

--- resources/js/myjs.js
+++ resources/js/myjs.js
@@ -132,3 +132,15 @@ function logKey(e) {
             break;
     }
 }
+
+
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+    // For Safari
+    return 'Sure?';
+};
